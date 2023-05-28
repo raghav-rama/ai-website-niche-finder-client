0afe67bd66446fb8501a2887279fd644c026d6a7
raghav-rama <hackerer528@gmail.com>
2023-05-28 09:36:26 +0530

    feat: add path alias

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
       '@utils': '/src/utils',
       '@hooks': '/src/hooks',
       '@assets': '/src/assets',
+      '@context': '/src/context',
     },
   },  
   css: {
